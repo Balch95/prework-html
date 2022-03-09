d85aec8f6c3a79512fd97cf15aec250250bcaa5e
Łukasz Woźniak <lukasz.wozniak95@hotmail.com>
2022-03-09 18:02:05 +0100

    Zadanie 40

---
+++ Zadanie 40/main.js
@@ -0,0 +1,9 @@
+function a(n){
+    if (n>=100 && n<=200) {
+        console.log('Liczba znajduję się w przedziale 100-200')
+    }else {
+        console.log('Liczba nie znajduję się w przedziale 100-200')
+    }
+}
+
+a(169);
