9be41d7f5d29edef8c9e0358541320ae56786eab
Łukasz Woźniak <lukasz.wozniak95@hotmail.com>
2022-03-09 16:44:19 +0100

    Zadanie 34

---
+++ Zadanie 34/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 10;
+let seconNumber = 5;
+
+function suma(num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+
+let wynik =suma(firstNumber, seconNumber);
+
+console.log(wynik);
+
