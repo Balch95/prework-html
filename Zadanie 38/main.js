d131416e21b8895cdc7f4e0c3c0de38c107dbd58
Łukasz Woźniak <lukasz.wozniak95@hotmail.com>
2022-03-09 17:26:10 +0100

    Zadanie 38

---
+++ Zadanie 38/main.js
@@ -0,0 +1,6 @@
+let zmienna = document.getElementById('opis');
+let button = document.querySelector('button');
+
+document.querySelector('button').onclick = function(){
+    zmienna.textContent = 'Dowolny tekts paragrafu opis';
+}
