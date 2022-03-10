ed60ccc33f61bef32bf24efd8c46e0bd9c95083e
Łukasz Woźniak <lukasz.wozniak95@hotmail.com>
2022-03-10 12:46:07 +0100

    zadanie 39 - poprawki

--- Zadanie 39/main.js
+++ Zadanie 39/main.js
@@ -2,13 +2,13 @@ let string1 = 'Uwielbiam JavaScript';
 let string2 = 'Jestem świetnym programistą - dobre sobie';
 
 function lstring(str1, str2){
-    if (str1.length>str2.length){
-        console.log('String 1 jest dłuższy');
+    if (str1.length > str2.length) {
+        return `String "${str1}" 1 jest dłuższy`;
     }else if(str1.length<str2.length){
-        console.log('String 2 jest dłuższy');
+        return `String "${str2}" jest dłuższy`;
     }else if(str1.length=str2.length){
-        console.log('Stringi mają taką samą długość')
+        return 'Stringi mają taką samą długość';
     }
 }
 
-lstring(string1, string2);
+console.log(lstring(string1, string2));
